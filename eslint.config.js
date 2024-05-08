f748e3a74ef2159d41ef7050441ff97df67af0fb
Fawad Ali <fawad@puffer.fi>
2024-05-08 15:25:15 +0200

    Add prettier config and ignore dist in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  {
+    ignores: ['dist'],
+  },
 ];
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      name: "puffer-smart-contract-lib",
-      entry: resolve(__dirname, "./lib/main.ts"),
-      fileName: "main",
-      formats: ["es", "cjs"],
+      name: 'puffer-smart-contract-lib',
+      entry: resolve(__dirname, './lib/main.ts'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
     },
   },
   plugins: [dts()],
