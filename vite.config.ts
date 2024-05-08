875709a6d68d9e50ecde3ccbaaf9bcbc3b762ce4
Fawad Ali <fawad@puffer.fi>
2024-05-08 15:12:29 +0200

    Configure ES and CJS builds with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
+      name: "puffer-smart-contract-lib",
       entry: resolve(__dirname, "./lib/main.ts"),
-      formats: ["es"],
+      fileName: "main",
+      formats: ["es", "cjs"],
     },
   },
+  plugins: [dts()],
 });
