c172fa3ad6d51170ebdd2d36b45a5ced0cba2f75
Fawad Ali <fawad@puffer.fi>
2024-05-13 20:37:50 +0200

    Fix addresses and add network configurations

--- lib/config/addresses.ts
+++ lib/config/addresses.ts
@@ -5,12 +5,11 @@ import anvilAddresses from './addresses/anvil.json';
 // Mainnet Addresses
 export const MAINNET_PUFFER_VAULT_CONTRACT_ADDRESS = mainnetAddresses.vault;
 export const MAINNET_PUFFER_PROTOCOL_CONTRACT_ADDRESS =
-  mainnetAddresses.protocol;
+  mainnetAddresses.pufferProtocolProxy;
 export const MAINNET_VALIDATOR_TICKET_CONTRACT_ADDRESS =
-  mainnetAddresses.validatorTicket;
+  mainnetAddresses.validatorTicketProxy;
 export const MAINNET_PUFFER_ORACLE_CONTRACT_ADDRESS = mainnetAddresses.oracle;
 export const MAINNET_GUARDIAN_MODULE_ADDRESS = mainnetAddresses.guardianModule;
-export const MAINNET_NO_RESTAKING_MODULE = mainnetAddresses.noRestakingModule;
 
 // Holesky Addresses
 export const HOLESKY_PUFFER_VAULT_CONTRACT_ADDRESS = holeskyAddresses.vault;
@@ -20,7 +19,6 @@ export const HOLESKY_VALIDATOR_TICKET_CONTRACT_ADDRESS =
   holeskyAddresses.validatorTicket;
 export const HOLESKY_PUFFER_ORACLE_CONTRACT_ADDRESS = holeskyAddresses.oracle;
 export const HOLESKY_GUARDIAN_MODULE_ADDRESS = holeskyAddresses.guardianModule;
-export const HOLESKY_NO_RESTAKING_MODULE = holeskyAddresses.noRestakingModule;
 
 // Anvil Addresses
 export const ANVIL_PUFFER_VAULT_CONTRACT_ADDRESS = anvilAddresses.vault;
@@ -29,4 +27,3 @@ export const ANVIL_VALIDATOR_TICKET_CONTRACT_ADDRESS =
   anvilAddresses.validatorTicket;
 export const ANVIL_PUFFER_ORACLE_CONTRACT_ADDRESS = anvilAddresses.oracle;
 export const ANVIL_GUARDIAN_MODULE_ADDRESS = anvilAddresses.guardianModule;
-export const ANVIL_NO_RESTAKING_MODULE = anvilAddresses.noRestakingModule;
