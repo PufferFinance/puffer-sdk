e4496229ce282b34ffeead4ba7421e0d471dd18f
Fawad Ali <fawad@puffer.fi>
2024-05-17 17:44:55 +0200

    Setup contract addresses and wallet connector

--- lib/api/puffer-client.ts
+++ lib/api/puffer-client.ts
@@ -1,9 +1,12 @@
 import {
   Address,
+  PublicClient,
   WalletClient,
+  createPublicClient,
   createWalletClient,
   custom,
   getContract,
+  http,
 } from 'viem';
 import { PufferVaultV2 } from '../contracts/abis/holesky/PufferVaultV2';
 import { Chain, VIEM_CHAINS } from '../chains/constants';
@@ -12,39 +15,39 @@ import { CHAIN_ADDRESSES } from '../contracts/addresses/addresses';
 export class PufferClient {
   private chain: Chain;
   private walletClient: WalletClient;
+  private publicClient: PublicClient;
 
   constructor(chain: Chain) {
     this.chain = chain;
+
     this.walletClient = createWalletClient({
       chain: VIEM_CHAINS[this.chain],
       transport: custom(window.ethereum!),
     });
+    this.publicClient = createPublicClient({
+      chain: VIEM_CHAINS[this.chain],
+      transport: http(),
+    });
   }
 
-  public async depositETH(value: bigint) {
-    if (!this.walletClient.account) {
-      throw new Error('Client account not found');
-    }
-
+  public async depositETH(walletAddress: Address, value: bigint) {
     const contract = getContract({
       address: CHAIN_ADDRESSES[this.chain].PufferVault as Address,
       abi: PufferVaultV2,
-      client: this.walletClient,
+      client: {
+        wallet: this.walletClient,
+        // Public client is needed for simulation.
+        public: this.publicClient,
+      },
     });
 
     // Simulating the contract to make sure everything works.
-    const { request } = await contract.simulate.depositETH(
-      [this.walletClient.account!.address],
-      {
-        account: this.walletClient.account!.address,
-        chain: VIEM_CHAINS[this.chain],
-        value,
-      },
-    );
+    const { request } = await contract.simulate.depositETH([walletAddress], {
+      account: walletAddress,
+      chain: VIEM_CHAINS[this.chain],
+      value,
+    });
 
-    return await contract.write.depositETH(
-      [this.walletClient.account!.address],
-      request,
-    );
+    return await contract.write.depositETH([walletAddress], request);
   }
 }
