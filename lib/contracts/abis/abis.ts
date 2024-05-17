f130d890970fdbaf63b5b9a853090f0b93dc0027
Fawad Ali <fawad@puffer.fi>
2024-05-17 20:58:04 +0200

    Implement a newer way to estimate and transact a function

---
+++ lib/contracts/abis/abis.ts
@@ -0,0 +1,16 @@
+import { Chain } from '../../chains/constants';
+import { PufferVaultV2 as MainNetPufferVaultV2 } from './mainnet/PufferVaultV2';
+import { PufferVaultV2 as HoleskyPufferVaultV2 } from './holesky/PufferVaultV2';
+import { PufferVaultV2 as AnvilPufferVaultV2 } from './anvil/PufferVaultV2';
+
+export const CHAIN_ABIS = {
+  [Chain.Mainnet]: {
+    PufferVaultV2: MainNetPufferVaultV2,
+  },
+  [Chain.Holesky]: {
+    PufferVaultV2: HoleskyPufferVaultV2,
+  },
+  [Chain.Anvil]: {
+    PufferVaultV2: AnvilPufferVaultV2,
+  },
+};
