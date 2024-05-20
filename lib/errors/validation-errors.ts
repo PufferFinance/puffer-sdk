2f26aa8f2a302c6821d51553b4ecc5b096950e81
Fawad Ali <fawad@puffer.fi>
2024-05-20 12:57:32 +0200

    Handle errors better

--- lib/errors/validation-errors.ts
+++ lib/errors/validation-errors.ts
@@ -1,15 +1,3 @@
-import { ErrorBaseParameters } from './types';
+import { BaseError } from "./base-error";
 
-export class AccountError extends Error {
-  constructor(message: string, params?: ErrorBaseParameters) {
-    super(message);
-
-    this.message = [
-      message,
-      '',
-      ...(params
-        ? Object.entries(params).map(([key, value]) => `${key}:  ${value}`)
-        : []),
-    ].join('\n');
-  }
-}
+export class AccountError extends BaseError {}
