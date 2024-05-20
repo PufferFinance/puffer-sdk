ef50b7866cb9041c1c68958caa6330258a221467
Fawad Ali <fawad@puffer.fi>
2024-05-20 12:57:32 +0200

    Handle errors better

---
+++ lib/errors/base-error.ts
@@ -0,0 +1,25 @@
+import { version } from '../utils/version';
+import { ErrorBaseParameters } from './types';
+
+// The parameters for the error can be extended for specific error types
+// to include more information.
+export abstract class BaseError<
+  T extends { [key: string]: string } = ErrorBaseParameters,
+> extends Error {
+  constructor(message: string, params: T) {
+    super(message);
+    this.message = this.compileMessage(message, params);
+  }
+
+  protected compileMessage(message: string, params: T) {
+    return [
+      message,
+      ...(params ? [''] : []),
+      ...(params
+        ? Object.entries(params).map(([key, value]) => `${key}: ${value}`)
+        : []),
+      '',
+      `v${version}`,
+    ].join('\n');
+  }
+}
