4e54f968004497595f06166d8699cfc5ca226e7b
Fawad Ali <fawad@puffer.fi>
2024-05-21 14:38:28 +0200

    Don't update dist

--- dist/main.cjs
+++ dist/main.cjs
@@ -1,4 +1,1 @@
-'use strict';
-Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-const d = (e, t) => e + t;
-exports.add = d;
+"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const d=(e,t)=>e+t;exports.add=d;
