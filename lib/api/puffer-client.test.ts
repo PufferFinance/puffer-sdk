a04981ab30fcff8e90afecafd8bd3959ee84b9c3
Fawad Ali <fawad@puffer.fi>
2024-05-22 13:27:21 +0200

    FINALLY make tests for depositETH work

--- lib/api/puffer-client.test.ts
+++ lib/api/puffer-client.test.ts
@@ -1,7 +1,11 @@
 import { Chain } from '../chains/constants';
 import { AccountError } from '../errors/validation-errors';
-import { setupTestWalletClient } from '../../test/utills/setup-test-clients';
+import {
+  setupMockPublicClient,
+  setupMockWalletClient,
+} from '../../test/mocks/setup-mock-clients';
 import { PufferClient } from './puffer-client';
+import { mockRpcRequest } from '../../test/mocks/mock-request';
 
 describe('PufferClient', () => {
   it('should throw error if the environment is invalid', async () => {
@@ -12,14 +16,38 @@ describe('PufferClient', () => {
     it('should request addresses', async () => {
       const mockAddress = '0xEB77D02f8122B32273444a1b544C147Fb559CB41';
 
-      const walletRequest = jest.fn().mockReturnValue([mockAddress]);
-      const walletClient = setupTestWalletClient(walletRequest);
+      const walletRequest = mockRpcRequest({
+        eth_requestAccounts: [mockAddress],
+      });
+      const walletClient = setupMockWalletClient(walletRequest);
 
       const pufferClient = new PufferClient(Chain.Anvil, walletClient);
       const [address] = await pufferClient.requestAddresses();
 
       expect(address).toBe(mockAddress);
       expect(walletRequest).toHaveBeenCalled();
+    });
+
+    it('should deposit ETH', async () => {
+      const mockAddress = '0xEB77D02f8122B32273444a1b544C147Fb559CB41';
+      const mockGas = BigInt(1);
+
+      const walletRequest = mockRpcRequest({
+        eth_sendTransaction: mockAddress,
+      });
+      const walletClient = setupMockWalletClient(walletRequest);
+      const publicRequest = mockRpcRequest({ eth_estimateGas: mockGas });
+      const publicClient = setupMockPublicClient(publicRequest);
+
+      const pufferClient = new PufferClient(
+        Chain.Anvil,
+        walletClient,
+        publicClient,
+      );
+      const { transact, estimate } = pufferClient.depositETH(mockAddress);
+
+      expect(await transact(BigInt(1))).toBe(mockAddress);
+      expect(await estimate()).toBe(mockGas);
     });
   });
 });
---
+++ test/utils/types.ts
@@ -0,0 +1,1 @@
+export type MockRequestFn = (...args: any) => Promise<any>;
