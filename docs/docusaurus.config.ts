0fac47725944ad7cbfe65d8598a167abac0f90be
Fawad Ali <fawad@puffer.fi>
2024-05-23 19:44:56 +0200

    Add a guides category and add guide for depositing ETH

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -57,12 +57,6 @@ const config: Config = {
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {
           href: 'https://github.com/PufferFinance',
           label: 'GitHub',
           position: 'right',
