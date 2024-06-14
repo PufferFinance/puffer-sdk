b08741a43495af21c636ee9741accf757846df5f
Fawad Ali <fawad@puffer.fi>
2024-06-14 15:11:29 +0200

    Add tests for TokensHandler and extend setup of test clients

--- test/setup-test-clients.ts
+++ test/setup-test-clients.ts
@@ -6,24 +6,42 @@ import {
   WalletClient,
   PublicClient,
 } from 'viem';
-import { anvil } from 'viem/chains';
-import { TransportProvider } from '../lib/utils/types';
+import { holesky } from 'viem/chains';
 import { testingUtils } from './setup-tests';
+import { TransportProvider } from '../lib/utils/types';
 
-export const setupTestClient = () => {
+export const setupTestClient = (
+  rpcRequestMock?: TransportProvider['request'],
+) => {
   return createTestClient({
     mode: 'anvil',
-    chain: anvil,
-    transport: custom(testingUtils.getProvider()),
+    chain: holesky,
+    transport: custom(
+      rpcRequestMock === undefined
+        ? testingUtils.getProvider()
+        : {
+            request: (...args: any[]) => {
+              if (args[0].method === 'eth_chainId') {
+                return Promise.resolve(holesky.id);
+              }
+
+              return rpcRequestMock(...args);
+            },
+          },
+    ),
   });
 };
 
-export const setupTestWalletClient = () => {
-  const client = setupTestClient();
+export const setupTestWalletClient = (
+  rpcRequestMock?: TransportProvider['request'],
+) => {
+  const client = setupTestClient(rpcRequestMock);
   return client.extend(walletActions) as WalletClient;
 };
 
-export const setupTestPublicClient = () => {
-  const client = setupTestClient();
+export const setupTestPublicClient = (
+  rpcRequestMock?: TransportProvider['request'],
+) => {
+  const client = setupTestClient(rpcRequestMock);
   return client.extend(publicActions) as PublicClient;
 };
