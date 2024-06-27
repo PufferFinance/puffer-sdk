5c9b42c00a20cbc06058946e447ba87976c15329
Fawad Ali <fawad@puffer.fi>
2024-06-27 11:56:39 +0200

    Setup a handler for processing PufToken contract using idfferent tokens

--- lib/contracts/addresses.ts
+++ lib/contracts/addresses.ts
@@ -6,13 +6,9 @@ export const CHAIN_ADDRESSES = {
   [Chain.Mainnet]: {
     PufferVault: '0xD9A442856C234a39a81a089C06451EBAa4306a72',
     PufferDepositor: '0x4aa799c5dfc01ee7d790e3bf1a7c2257ce1dceff',
-    // TODO: Change this to the correct address once contract is
-    // deployed on mainnet.
-    PufToken: '0xfcf6c4e0387a523b73691d5604e5a6da1607c8a0',
   },
   [Chain.Holesky]: {
     PufferVault: '0x9196830bB4c05504E0A8475A0aD566AceEB6BeC9',
     PufferDepositor: '0x824AC05aeb86A0aD770b8acDe0906d2d4a6c4A8c',
-    PufToken: '0xfcf6c4e0387a523b73691d5604e5a6da1607c8a0',
   },
 };
