c9099be7e7e2a1e433b83d5e8cc1301442acf6ae
Fawad Ali <fawad@puffer.fi>
2024-07-01 10:33:00 +0200

    Add tests for PufferL2Depositor contract handler

---
+++ lib/contracts/handlers/puffer-l2-depositor-handler.test.ts
@@ -0,0 +1,77 @@
+import { Address, isHash } from 'viem';
+import {
+  setupTestWalletClient,
+  setupTestPublicClient,
+} from '../../../test/setup-test-clients';
+import { mockAccount, testingUtils } from '../../../test/setup-tests';
+import { Chain } from '../../chains/constants';
+import { PUFFER_L2_DEPOSITOR_ABIS } from '../abis/puffer-depositor-abis';
+import { PufferL2DepositorHandler } from './puffer-l2-depositor-handler';
+import { Token } from '../tokens';
+
+describe('PufferL2DepositorHandler', () => {
+  const contractTestingUtils = testingUtils.generateContractUtils(
+    PUFFER_L2_DEPOSITOR_ABIS[Chain.Holesky].PufferL2Depositor,
+  );
+  let handler: PufferL2DepositorHandler;
+
+  beforeEach(() => {
+    const walletClient = setupTestWalletClient();
+    const publicClient = setupTestPublicClient();
+
+    handler = new PufferL2DepositorHandler(
+      Chain.Holesky,
+      walletClient,
+      publicClient,
+    );
+  });
+
+  // TODO: This test is not working because the suggested parameters
+  // from the PufferL2Depositor contract are not exactly correct. See
+  // https://github.com/PufferFinance/puffer-contracts/blob/d3e318f3c45d744bfa2dbadfa1abe998fa49d4b5/mainnet-contracts/src/interface/IPufferL2Depositor.sol#L51-L57
+
+  // it('should deposit pre-approved token', async () => {
+  // contractTestingUtils.mockTransaction('deposit');
+
+  //   const { transact, estimate } = handler.depositAfterApproval(
+  //     Token.stETH,
+  //     mockAccount,
+  //     10n,
+  //   );
+
+  //   expect(typeof (await estimate())).toBe('bigint');
+  //   expect(isHash(await transact())).toBe(true);
+  // });
+
+  it('should deposit token after requesting permit', async () => {
+    contractTestingUtils.mockTransaction('deposit');
+    const mockPermitSignature = {
+      r: `0x${new Array(64).fill(0).join('')}` as Address,
+      s: `0x${new Array(64).fill(0).join('')}` as Address,
+      v: 0n,
+      yParity: 1,
+      deadline: 0n,
+    };
+    jest
+      .spyOn((handler as any).tokensHandler, 'getPermitSignature')
+      .mockReturnValue(Promise.resolve(mockPermitSignature));
+
+    const { transact, estimate } = await handler.deposit(
+      Token.stETH,
+      mockAccount,
+      10n,
+    );
+
+    expect(typeof (await estimate())).toBe('bigint');
+    expect(isHash(await transact())).toBe(true);
+  });
+
+  it('should deposit ETH', async () => {
+    contractTestingUtils.mockTransaction('depositETH');
+
+    const { transact, estimate } = handler.depositETH(mockAccount, 123n);
+
+    expect(typeof (await estimate())).toBe('bigint');
+    expect(isHash(await transact())).toBe(true);
+  });
+});
