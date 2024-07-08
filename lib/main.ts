22008268bc9ad02ceb7ba166d4fcd6d367f5df3d
Fawad Ali <fawad@puffer.fi>
2024-07-08 16:56:46 +0200

    Export contract addresses so they can be used in approval

--- lib/main.ts
+++ lib/main.ts
@@ -2,3 +2,4 @@ export * from './api/puffer-client';
 export * from './api/puffer-client-helpers';
 export * from './chains/constants';
 export * from './contracts/tokens';
+export * from './contracts/addresses';
