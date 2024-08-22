74aea3134603dd4f07c3639f066228126b8b55c7
Fawad Ali <fawad@puffer.fi>
2024-08-22 14:01:45 +0200

    Add preliminary docs for handlers

--- lib/contracts/addresses.ts
+++ lib/contracts/addresses.ts
@@ -6,16 +6,12 @@ export const CONTRACT_ADDRESSES = {
   [Chain.Mainnet]: {
     PufferVault: '0xD9A442856C234a39a81a089C06451EBAa4306a72',
     PufferDepositor: '0x4aa799c5dfc01ee7d790e3bf1a7c2257ce1dceff',
-    // TODO: Update the addresses once the contracts are deployed on chain.
-    // See https://dev.azure.com/pufferfi/Frontend/_workitems/edit/1797.
     PufferL2Depositor: '0x3436E0B85cd929929F5802e792CFE282166E0259',
     PufLocker: '0x48e8dE138C246c14248C94d2D616a2F9eb4590D2',
   },
   [Chain.Holesky]: {
     PufferVault: '0x9196830bB4c05504E0A8475A0aD566AceEB6BeC9',
     PufferDepositor: '0x824AC05aeb86A0aD770b8acDe0906d2d4a6c4A8c',
-    // TODO: Update the addresses once the contracts are deployed on chain.
-    // See https://dev.azure.com/pufferfi/Frontend/_workitems/edit/1797.
     PufferL2Depositor: '0x0af6998e4828ad8ef8f79a9288d0a861890f791d',
     PufLocker: '0xa58983ad0899a452b7420bc57228e329d7ba92b6',
   },
