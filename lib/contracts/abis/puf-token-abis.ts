9e2017efc74488999bdac4c79f41aa44e6a56523
Fawad Ali <fawad@puffer.fi>
2024-10-01 15:43:10 +0200

    Remove puf token contract and add boring vault from nucleus

--- lib/contracts/abis/puf-token-abis.ts
+++
@@ -1,7 +0,0 @@
-import { Chain } from '../../chains/constants';
-import { PufToken } from './mainnet/PufToken';
-
-export const PUF_TOKEN_ABIS = {
-  [Chain.Mainnet]: { PufToken },
-  [Chain.Holesky]: { PufToken },
-};
