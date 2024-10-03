be853539ad15162be10270d21c44d1da1340ff9b
Fawad Ali <fawad@puffer.fi>
2024-10-03 11:44:27 +0200

    Add tests for NucleusAccountantHandler

---
+++ lib/contracts/handlers/nucleus-accountant-handler.test.ts
@@ -0,0 +1,75 @@
+import {
+  setupTestPublicClient,
+  setupTestWalletClient,
+} from '../../../test/setup-test-clients';
+import { NUCLEUS_ACCOUNTANT_ABIS } from '../abis/nucleus-accountant-abis';
+import { Chain } from '../../chains/constants';
+import { NucleusAccountantHandler } from './nucleus-accountant-handler';
+import { mockAccount, testingUtils } from '../../../test/setup-tests';
+import { generateAddress } from '../../../test/mocks/address';
+import { Address } from 'viem';
+
+describe('NucleusAccountantHandler', () => {
+  const contractTestingUtils = testingUtils.generateContractUtils(
+    NUCLEUS_ACCOUNTANT_ABIS[Chain.Mainnet].Accountant,
+  );
+  let handler: NucleusAccountantHandler;
+
+  beforeEach(() => {
+    testingUtils.mockConnectedWallet([mockAccount], { chainId: Chain.Mainnet });
+    const walletClient = setupTestWalletClient();
+    const publicClient = setupTestPublicClient();
+
+    handler = new NucleusAccountantHandler(
+      Chain.Mainnet,
+      walletClient,
+      publicClient,
+    );
+  });
+
+  it('should get the rate provider data', async () => {
+    const tokenAddress = generateAddress();
+    const mockRateProviderData = {
+      isPeggedToBase: true,
+      rateProvider: generateAddress(),
+    };
+    contractTestingUtils.mockCall('rateProviderData', [
+      mockRateProviderData.isPeggedToBase,
+      mockRateProviderData.rateProvider,
+    ]);
+
+    const rateProviderData = await handler.rateProviderData(tokenAddress);
+    rateProviderData.rateProvider =
+      rateProviderData.rateProvider.toLowerCase() as Address;
+    expect(rateProviderData).toEqual(mockRateProviderData);
+  });
+
+  it('should get the vault', async () => {
+    const mockVault = generateAddress();
+    contractTestingUtils.mockCall('vault', [mockVault]);
+
+    const vault = await handler.vault();
+    expect(vault.toLowerCase()).toEqual(mockVault);
+  });
+
+  it('should get the different types of rates', async () => {
+    const mockRate = 100n;
+    const tokenAddress = generateAddress();
+
+    contractTestingUtils.mockCall('getRate', [mockRate]);
+    const rate = await handler.getRate();
+    expect(rate).toEqual(mockRate);
+
+    contractTestingUtils.mockCall('getRateSafe', [mockRate]);
+    const rateSafe = await handler.getRateSafe();
+    expect(rateSafe).toEqual(mockRate);
+
+    contractTestingUtils.mockCall('getRateInQuote', [mockRate]);
+    const rateInQuote = await handler.getRateInQuote(tokenAddress);
+    expect(rateInQuote).toEqual(mockRate);
+
+    contractTestingUtils.mockCall('getRateInQuoteSafe', [mockRate]);
+    const rateInQuoteSafe = await handler.getRateInQuoteSafe(tokenAddress);
+    expect(rateInQuoteSafe).toEqual(mockRate);
+  });
+});
