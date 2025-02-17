55aeede670445517144dfcdf9668b762c19f7e35
Fawad Ali <fawad@puffer.fi>
2025-02-17 18:09:17 +0100

    Add handler and tests for CarrotStaker contract

---
+++ lib/contracts/handlers/carrot-staker-handler.test.ts
@@ -0,0 +1,89 @@
+import { PublicClient, WalletClient } from 'viem';
+import { Chain } from '../../chains/constants';
+import {
+  setupTestWalletClient,
+  setupTestPublicClient,
+} from '../../../test/setup-test-clients';
+import { mockAccount, testingUtils } from '../../../test/setup-tests';
+import { CarrotStakerHandler } from './carrot-staker-handler';
+import { CarrotStaker } from '../abis/mainnet/CarrotStaker';
+
+describe('CarrotStakerHandler', () => {
+  const contractTestingUtils = testingUtils.generateContractUtils(CarrotStaker);
+  let handler: CarrotStakerHandler;
+  let walletClient: WalletClient;
+  let publicClient: PublicClient;
+
+  beforeEach(() => {
+    walletClient = setupTestWalletClient();
+    publicClient = setupTestPublicClient();
+
+    handler = new CarrotStakerHandler(
+      Chain.Holesky,
+      walletClient,
+      publicClient,
+    );
+  });
+
+  it('should get the allowance', async () => {
+    const mockAllowance = 10n;
+    contractTestingUtils.mockCall('allowance', [mockAllowance]);
+
+    const allowance = await handler.allowance(mockAccount, mockAccount);
+    expect(allowance).toBe(mockAllowance);
+  });
+
+  it('should get the balance of the address', async () => {
+    const mockBalance = 10n;
+    contractTestingUtils.mockCall('balanceOf', [mockBalance]);
+
+    const balance = await handler.balanceOf(mockAccount);
+    expect(balance).toBe(mockBalance);
+  });
+
+  it('should get the number of decimals', async () => {
+    const mockDecimals = 18;
+    contractTestingUtils.mockCall('decimals', [mockDecimals]);
+
+    const decimals = await handler.decimals();
+    expect(decimals).toBe(mockDecimals);
+  });
+
+  it('should get whether unstaking is allowed', async () => {
+    const mockIsUnstakingAllowed = true;
+    contractTestingUtils.mockCall('isUnstakingAllowed', [
+      mockIsUnstakingAllowed,
+    ]);
+
+    const isUnstakingAllowed = await handler.isUnstakingAllowed();
+    expect(isUnstakingAllowed).toBe(mockIsUnstakingAllowed);
+  });
+
+  it('should get the total supply', async () => {
+    const mockTotalSupply = 10n;
+    contractTestingUtils.mockCall('totalSupply', [mockTotalSupply]);
+
+    const totalSupply = await handler.totalSupply();
+    expect(totalSupply).toBe(mockTotalSupply);
+  });
+
+  it('should stake CARROT tokens', async () => {
+    const mockAmount = 10n;
+    contractTestingUtils.mockTransaction('stake');
+
+    const stakeTx = await handler.stake(mockAccount, mockAmount);
+    expect(stakeTx).toBeDefined();
+  });
+
+  it('should unstake CARROT tokens', async () => {
+    const mockAmount = 10n;
+    contractTestingUtils.mockTransaction('unstake');
+
+    const unstakeTx = await handler.unstake(
+      mockAccount,
+      mockAmount,
+      mockAccount,
+    );
+    expect(unstakeTx).toBeDefined();
+  });
+});
---
+++ lib/contracts/handlers/carrot-staker-handler.ts
@@ -0,0 +1,130 @@
+import {
+  Address,
+  WalletClient,
+  PublicClient,
+  getContract,
+  GetContractReturnType,
+} from 'viem';
+import { ViemChain, VIEM_CHAINS, Chain } from '../../chains/constants';
+import { CONTRACT_ADDRESSES } from '../addresses';
+import { CarrotStaker } from '../abis/mainnet/CarrotStaker';
+
+/**
+ * Handler for the `CarrotStaker` contract.
+ */
+export class CarrotStakerHandler {
+  private viemChain: ViemChain;
+
+  /**
+   * Create the handler for the `CarrotStaker` contract exposing
+   * methods to interact with the contract.
+   *
+   * @param chain Chain to use for the client.
+   * @param walletClient The wallet client to use for wallet
+   * interactions.
+   * @param publicClient The public client to use for public
+   * interactions.
+   */
+  constructor(
+    private chain: Chain,
+    private walletClient: WalletClient,
+    private publicClient: PublicClient,
+  ) {
+    this.viemChain = VIEM_CHAINS[chain];
+  }
+
+  /**
+   * Get the contract. This is a method because the typings are complex
+   * and lost when trying to make it a member.
+   *
+   * @returns The viem contract.
+   */
+  public getContract() {
+    const address = CONTRACT_ADDRESSES[this.chain].CarrotStaker as Address;
+    const abi = CarrotStaker;
+    const client = { public: this.publicClient, wallet: this.walletClient };
+
+    return getContract({ address, abi, client }) as GetContractReturnType<
+      typeof abi,
+      typeof client,
+      Address
+    >;
+  }
+
+  /**
+   * Get the allowance for the spender to spend the owner's tokens.
+   *
+   * @param owner The owner of the tokens.
+   * @param spender The spender of the tokens.
+   * @returns The allowance of the owner for the spender.
+   */
+  public allowance(owner: Address, spender: Address) {
+    return this.getContract().read.allowance([owner, spender]);
+  }
+
+  /**
+   * Get the balance of the address.
+   *
+   * @param address The address to get the balance of.
+   * @returns The balance of the address.
+   */
+  public balanceOf(address: Address) {
+    return this.getContract().read.balanceOf([address]);
+  }
+
+  /**
+   * Get the number of decimals of the token.
+   *
+   * @returns The number of decimals of the token.
+   */
+  public decimals() {
+    return this.getContract().read.decimals();
+  }
+
+  /**
+   * Get whether unstaking is allowed.
+   *
+   * @returns Whether unstaking is allowed.
+   */
+  public isUnstakingAllowed() {
+    return this.getContract().read.isUnstakingAllowed();
+  }
+
+  /**
+   * Get the total supply of the token.
+   *
+   * @returns The total supply of the token.
+   */
+  public totalSupply() {
+    return this.getContract().read.totalSupply();
+  }
+
+  /**
+   * Stake CARROT tokens.
+   *
+   * @param account The account to stake CARROT tokens from.
+   * @param amount The amount of CARROT tokens to stake.
+   * @returns The transaction hash.
+   */
+  public stake(account: Address, amount: bigint) {
+    return this.getContract().write.stake([amount], {
+      account,
+      chain: this.viemChain,
+    });
+  }
+
+  /**
+   * Unstake CARROT tokens.
+   *
+   * @param account The account to unstake CARROT tokens from.
+   * @param amount The amount of CARROT tokens to unstake.
+   * @param recipient The recipient of the unstaked tokens.
+   * @returns The transaction hash.
+   */
+  public unstake(account: Address, amount: bigint, recipient: Address) {
+    return this.getContract().write.unstake([amount, recipient], {
+      account,
+      chain: this.viemChain,
+    });
+  }
+}
