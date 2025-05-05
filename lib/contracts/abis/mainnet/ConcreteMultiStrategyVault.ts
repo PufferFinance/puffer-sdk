79f3058d54f22d5b99242dfbbe059a04fde6e3d5
Nick Guest <nicholas.guest@gmail.com>
2025-05-05 12:10:50 +0200

    3552: remove unused ABI functions

--- lib/contracts/abis/mainnet/ConcreteMultiStrategyVault.ts
+++ lib/contracts/abis/mainnet/ConcreteMultiStrategyVault.ts
@@ -10,25 +10,6 @@ export const ConcreteMultiStrategyVault = <const>[
     type: 'error',
   },
   {
-    inputs: [
-      { internalType: 'address', name: 'sender', type: 'address' },
-      { internalType: 'uint256', name: 'balance', type: 'uint256' },
-      { internalType: 'uint256', name: 'needed', type: 'uint256' },
-    ],
-    name: 'ERC20InsufficientBalance',
-    type: 'error',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'approver', type: 'address' }],
-    name: 'ERC20InvalidApprover',
-    type: 'error',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'spender', type: 'address' }],
-    name: 'ERC20InvalidSpender',
-    type: 'error',
-  },
-  {
     anonymous: false,
     inputs: [
       {
