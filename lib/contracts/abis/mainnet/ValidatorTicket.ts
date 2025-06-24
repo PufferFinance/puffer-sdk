61d1a8f7cbe1294193efc7a6902cbc73fd106e42
Nick Guest <nicholas.guest@gmail.com>
2025-06-24 12:16:11 +0200

    reduce VT ABI to only esssential fns

--- lib/contracts/abis/mainnet/ValidatorTicket.ts
+++ lib/contracts/abis/mainnet/ValidatorTicket.ts
@@ -34,65 +34,6 @@ export const ValidatorTicket = <const>[
   },
   {
     type: 'function',
-    name: 'GUARDIAN_MODULE',
-    inputs: [],
-    outputs: [
-      {
-        name: '',
-        type: 'address',
-        internalType: 'address payable',
-      },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'PUFFER_ORACLE',
-    inputs: [],
-    outputs: [
-      {
-        name: '',
-        type: 'address',
-        internalType: 'contract IPufferOracle',
-      },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'PUFFER_VAULT',
-    inputs: [],
-    outputs: [
-      {
-        name: '',
-        type: 'address',
-        internalType: 'address payable',
-      },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'TREASURY',
-    inputs: [],
-    outputs: [
-      {
-        name: '',
-        type: 'address',
-        internalType: 'address payable',
-      },
-    ],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
-    name: 'UPGRADE_INTERFACE_VERSION',
-    inputs: [],
-    outputs: [{ name: '', type: 'string', internalType: 'string' }],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
     name: 'allowance',
     inputs: [
       {
@@ -126,13 +67,6 @@ export const ValidatorTicket = <const>[
     ],
     outputs: [{ name: '', type: 'bool', internalType: 'bool' }],
     stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'authority',
-    inputs: [],
-    outputs: [{ name: '', type: 'address', internalType: 'address' }],
-    stateMutability: 'view',
   },
   {
     type: 'function',
@@ -222,36 +156,6 @@ export const ValidatorTicket = <const>[
   },
   {
     type: 'function',
-    name: 'initialize',
-    inputs: [
-      {
-        name: 'accessManager',
-        type: 'address',
-        internalType: 'address',
-      },
-      {
-        name: 'treasuryFeeRate',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-      {
-        name: 'guardiansFeeRate',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'isConsumingScheduledOp',
-    inputs: [],
-    outputs: [{ name: '', type: 'bytes4', internalType: 'bytes4' }],
-    stateMutability: 'view',
-  },
-  {
-    type: 'function',
     name: 'name',
     inputs: [],
     outputs: [{ name: '', type: 'string', internalType: 'string' }],
@@ -300,13 +204,6 @@ export const ValidatorTicket = <const>[
     ],
     outputs: [],
     stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'proxiableUUID',
-    inputs: [],
-    outputs: [{ name: '', type: 'bytes32', internalType: 'bytes32' }],
-    stateMutability: 'view',
   },
   {
     type: 'function',
@@ -397,45 +294,6 @@ export const ValidatorTicket = <const>[
   },
   {
     type: 'function',
-    name: 'setAuthority',
-    inputs: [
-      {
-        name: 'newAuthority',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'setGuardiansFeeRate',
-    inputs: [
-      {
-        name: 'newGuardiansFeeRate',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
-    name: 'setProtocolFeeRate',
-    inputs: [
-      {
-        name: 'newProtocolFeeRate',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-    outputs: [],
-    stateMutability: 'nonpayable',
-  },
-  {
-    type: 'function',
     name: 'symbol',
     inputs: [],
     outputs: [{ name: '', type: 'string', internalType: 'string' }],
@@ -482,20 +340,6 @@ export const ValidatorTicket = <const>[
     stateMutability: 'nonpayable',
   },
   {
-    type: 'function',
-    name: 'upgradeToAndCall',
-    inputs: [
-      {
-        name: 'newImplementation',
-        type: 'address',
-        internalType: 'address',
-      },
-      { name: 'data', type: 'bytes', internalType: 'bytes' },
-    ],
-    outputs: [],
-    stateMutability: 'payable',
-  },
-  {
     type: 'event',
     name: 'Approval',
     inputs: [
@@ -513,126 +357,6 @@ export const ValidatorTicket = <const>[
       },
       {
         name: 'value',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'AuthorityUpdated',
-    inputs: [
-      {
-        name: 'authority',
-        type: 'address',
-        indexed: false,
-        internalType: 'address',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'DispersedETH',
-    inputs: [
-      {
-        name: 'treasury',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'guardians',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'vault',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'DispersedPufETH',
-    inputs: [
-      {
-        name: 'treasury',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'guardians',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'burned',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'EIP712DomainChanged',
-    inputs: [],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'GuardiansFeeChanged',
-    inputs: [
-      {
-        name: 'oldGuardiansFee',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'newGuardiansFee',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'Initialized',
-    inputs: [
-      {
-        name: 'version',
-        type: 'uint64',
-        indexed: false,
-        internalType: 'uint64',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'ProtocolFeeChanged',
-    inputs: [
-      {
-        name: 'oldTreasuryFee',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-      {
-        name: 'newTreasuryFee',
         type: 'uint256',
         indexed: false,
         internalType: 'uint256',
@@ -665,123 +389,6 @@ export const ValidatorTicket = <const>[
     ],
     anonymous: false,
   },
-  {
-    type: 'event',
-    name: 'TransferredETH',
-    inputs: [
-      {
-        name: 'to',
-        type: 'address',
-        indexed: true,
-        internalType: 'address',
-      },
-      {
-        name: 'amount',
-        type: 'uint256',
-        indexed: false,
-        internalType: 'uint256',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'event',
-    name: 'Upgraded',
-    inputs: [
-      {
-        name: 'implementation',
-        type: 'address',
-        indexed: true,
-        internalType: 'address',
-      },
-    ],
-    anonymous: false,
-  },
-  {
-    type: 'error',
-    name: 'AccessManagedInvalidAuthority',
-    inputs: [
-      {
-        name: 'authority',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'AccessManagedRequiredDelay',
-    inputs: [
-      {
-        name: 'caller',
-        type: 'address',
-        internalType: 'address',
-      },
-      { name: 'delay', type: 'uint32', internalType: 'uint32' },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'AccessManagedUnauthorized',
-    inputs: [
-      {
-        name: 'caller',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'AddressEmptyCode',
-    inputs: [
-      {
-        name: 'target',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'AddressInsufficientBalance',
-    inputs: [
-      {
-        name: 'account',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  { type: 'error', name: 'ECDSAInvalidSignature', inputs: [] },
-  {
-    type: 'error',
-    name: 'ECDSAInvalidSignatureLength',
-    inputs: [
-      {
-        name: 'length',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'ECDSAInvalidSignatureS',
-    inputs: [{ name: 's', type: 'bytes32', internalType: 'bytes32' }],
-  },
-  {
-    type: 'error',
-    name: 'ERC1967InvalidImplementation',
-    inputs: [
-      {
-        name: 'implementation',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  { type: 'error', name: 'ERC1967NonPayable', inputs: [] },
   {
     type: 'error',
     name: 'ERC20InsufficientAllowance',
@@ -837,28 +444,6 @@ export const ValidatorTicket = <const>[
   },
   {
     type: 'error',
-    name: 'ERC20InvalidReceiver',
-    inputs: [
-      {
-        name: 'receiver',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'ERC20InvalidSender',
-    inputs: [
-      {
-        name: 'sender',
-        type: 'address',
-        internalType: 'address',
-      },
-    ],
-  },
-  {
-    type: 'error',
     name: 'ERC20InvalidSpender',
     inputs: [
       {
@@ -895,49 +480,6 @@ export const ValidatorTicket = <const>[
       },
     ],
   },
-  { type: 'error', name: 'FailedInnerCall', inputs: [] },
-  {
-    type: 'error',
-    name: 'InvalidAccountNonce',
-    inputs: [
-      {
-        name: 'account',
-        type: 'address',
-        internalType: 'address',
-      },
-      {
-        name: 'currentNonce',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-  },
   { type: 'error', name: 'InvalidAmount', inputs: [] },
-  { type: 'error', name: 'InvalidData', inputs: [] },
-  { type: 'error', name: 'InvalidInitialization', inputs: [] },
-  { type: 'error', name: 'MathOverflowedMulDiv', inputs: [] },
-  { type: 'error', name: 'NotInitializing', inputs: [] },
   { type: 'error', name: 'RecipientIsZeroAddress', inputs: [] },
-  {
-    type: 'error',
-    name: 'SafeCastOverflowedUintDowncast',
-    inputs: [
-      { name: 'bits', type: 'uint8', internalType: 'uint8' },
-      {
-        name: 'value',
-        type: 'uint256',
-        internalType: 'uint256',
-      },
-    ],
-  },
-  {
-    type: 'error',
-    name: 'UUPSUnauthorizedCallContext',
-    inputs: [],
-  },
-  {
-    type: 'error',
-    name: 'UUPSUnsupportedProxiableUUID',
-    inputs: [{ name: 'slot', type: 'bytes32', internalType: 'bytes32' }],
-  },
 ];
