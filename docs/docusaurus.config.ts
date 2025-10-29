223ebcacaaa7a75a3bf8ba08116f6393411fc0ab
Fawad Ali <fawad@puffer.fi>
2025-10-29 13:26:22 +0100

    Update all packages and fix vulnerabilities

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,7 +19,6 @@ const config: Config = {
   projectName: 'puffer-sdk', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -31,6 +30,9 @@ const config: Config = {
 
   markdown: {
     format: 'detect',
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
   },
 
   presets: [
